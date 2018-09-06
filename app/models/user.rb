c2bbad353d2e1fc664a6b004c54c345263cd66f6
masashi.muramatsu <muramasa125@gmail.com>
2018-09-06 13:48:25 +0000

    add favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationships, source: :user
+  has_many :favorites
+  has_many :favorite_microposts, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -20,7 +22,7 @@ class User < ApplicationRecord
   end
   
   def unfollow(other_user)
-    relationship = self.relationships.find_by(follow_id: other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
     relationship.destroy if relationship
   end
   
@@ -29,6 +31,19 @@ class User < ApplicationRecord
   end
   
   def feed_microposts
-    Microposts.where(user_id: self.following_ids + [self.id])
+    Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def add_favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def remove_favorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorited?(micropost)
+    self.favorite_microposts.include?(micropost)
   end
 end
